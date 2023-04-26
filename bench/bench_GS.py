ffd36c561144a7ffb3913619e095fb7898885991
David Sénéchal <david.senechal@usherbrooke.ca>
2023-04-25 20:42:14 -0400

    Added more bench files. Removed obsolete files from tracking.

--- bench/bench_GS.py
+++ bench/bench_GS.py
@@ -5,8 +5,14 @@ import timeit
 
 pyqcm.set_global_parameter('verb_ED')
 pyqcm.set_global_parameter('verb_Hilbert')
+pyqcm.set_global_parameter('Ground_state_method', 'L')
+pyqcm.set_global_parameter('PRIMME_algorithm', 1)
 pyqcm.set_global_parameter('parallel_sectors')
-F = 'S'
+# pyqcm.set_global_parameter('Davidson_states', 2)
+# pyqcm.set_global_parameter('PRIMME_preconditionning', 1)
+
+# pyqcm.set_global_parameter('CSR_sym_store')
+F = 'E'
 L = 14
 model = model1D(L,sym=True)
 pyqcm.set_global_parameter('Hamiltonian_format', F)
@@ -19,7 +25,6 @@ model.set_parameters("""
 I = pyqcm.model_instance(model)
 T = timeit.default_timer()
 print("ground state : ", I.ground_state())
-I.cluster_Green_function(0.1j, 0)
 T = timeit.default_timer() - T
 print('format {:s} : time = {:1.2f}'.format(F, T))
 
--- bench/bench_GS2.py
+++ bench/bench_GS2.py
@@ -1,16 +1,22 @@
 import pyqcm
 import numpy as np
-from model_1D import model1D
+from model_2D import model2D
 import timeit
 
 pyqcm.set_global_parameter('verb_ED')
 pyqcm.set_global_parameter('verb_Hilbert')
-pyqcm.set_global_parameter('parallel_sectors')
-F = 'S'
-L = 14
-model = model1D(L,sym=True)
+# pyqcm.set_global_parameter('Ground_state_method', 'L')
+# pyqcm.set_global_parameter('PRIMME_algorithm', 1)
+# pyqcm.set_global_parameter('Davidson_states', 2)
+# pyqcm.set_global_parameter('PRIMME_preconditionning', 1)
+
+# pyqcm.set_global_parameter('CSR_sym_store')
+F = 'E'
+Lx = 7
+Ly = 2
+model = model2D(Lx, Ly, sym=True)
 pyqcm.set_global_parameter('Hamiltonian_format', F)
-model.set_target_sectors('R0:N{:d}:S0'.format(L))
+model.set_target_sectors('R0:N{:d}:S0'.format(Lx*Ly))
 model.set_parameters("""
     U = 4
     t = 1
@@ -19,7 +25,6 @@ model.set_parameters("""
 I = pyqcm.model_instance(model)
 T = timeit.default_timer()
 print("ground state : ", I.ground_state())
-I.cluster_Green_function(0.1j, 0)
 T = timeit.default_timer() - T
 print('format {:s} : time = {:1.2f}'.format(F, T))
 

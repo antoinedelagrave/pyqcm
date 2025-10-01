ddb6818553e97de8123a84a10eec71e23a758765
David Sénéchal <david.senechal@usherbrooke.ca>
2025-10-01 16:01:37 -0400

    Minor differences in some test files.

--- tests/test_files/test_dispersion.py
+++ tests/test_files/test_dispersion.py
@@ -12,34 +12,7 @@ F = None
 # import record_2x2_anom
 # import record_spin
 
-# DoS(w=4, eta=0.2, file = F, plt_ax=ax)
-# Fermi_surface(file = F, plt_ax=ax)
-# G_dispersion(max=20, contour=True, file = F, plt_ax=ax)
-# Luttinger_surface(file = F, plt_ax=ax)
-# cluster_spectral_function(file = F, plt_ax=ax)
-# dispersion(file = F, contour=False, plt_ax=ax, labels=True)
-# mdc(file = F, plt_ax=ax)
-# momentum_profile('t', file = F, plt_ax=ax)
-# segment_dispersion(file = F, plt_ax=ax)
-# spectral_function(file = F, plt_ax=ax)
-# spectral_function_Lehmann(lims=(-5,5), file = F, plt_ax=ax)
-# spin_mdc(nk=20, orb=1, file = F, plt_ax=ax, opt='spins')
-# spin_mdc(nk=200, orb=1, file = F, plt_ax=ax, opt='spinp')
-# spin_mdc(nk=200, orb=1, file = F, plt_ax=ax, opt='sz')
-# spin_mdc(nk=25, orb=1, file = F, plt_ax=ax, opt='spins')
-# mdc_anomalous(orbs=(1,1), self=True, quadrant=True, file = F, plt_ax=ax)
-
-#-----------------------------------------------------------------
-# import record_2x2_8b
-
-# hybridization_function(file = F, plt_ax=ax)
-
-#-----------------------------------------------------------------
-# import record_2x2_anom
-
-# plt.show()
-
-
+# I.plot_dispersion(); exit()
 ##################################################################
 # TEST UNITAIRE
 
